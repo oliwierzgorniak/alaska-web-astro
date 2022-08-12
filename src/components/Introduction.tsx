9efe8b2ffc7d68117df3ac90a70221841644e513
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2022-08-12 14:58:34 +0200

    introduction styled

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,12 +1,23 @@
 const Introduction = () => (
-  <header>
-    <h1>
-      Hej, nazywam się Oliwier <br />
+  <header className="mx-6 grid grid-cols-[1fr_auto] grid-rows-[repeat(2,auto)]">
+    <h1 className="mr-[-1.5em] leading-9">
+      <span className="text-xl inline-block mb-1">
+        Hej, nazywam się Oliwier
+      </span>
+      <br />
       Zajmuję się tworzeniem stron internetowych
     </h1>
-    <img src="/images/me.png" alt="moje zdjęcie" />
-    <button>
-      <img src="/images/contact-button.svg" alt="grafika kontaktu ze mną" />
+    <img
+      className="w-48 col-start-2 row-start-1 row-end-3 self-end block"
+      src="/images/me.png"
+      alt="moje zdjęcie"
+    />
+    <button className="self-start justify-self-center">
+      <img
+        className="h-40"
+        src="/images/contact-button.svg"
+        alt="grafika kontaktu ze mną"
+      />
     </button>
   </header>
 );
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,7 +2,7 @@ import Language from "./nav/Language";
 import Menu from "./nav/Menu";
 
 const Nav = () => (
-  <nav className="m-6 flex justify-between">
+  <nav className="m-6 mb-36 flex justify-between">
     <img className="w-22" src="/images/logo-dark.svg" alt="logo Alaska Web" />
     <div className="flex gap-9">
       <Language />
