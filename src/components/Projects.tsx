3c362c05227898a994f4c1d19308fcfda2239204
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2022-08-17 17:51:29 +0200

    projects translation done

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,24 +1,34 @@
-import projects from "../data/projects.json";
+import projects from "../data/projects";
 import Project from "./projects/Project";
 
-const Projects = () => (
-  <section className="flex flex-col items-center px-6 py-16" id="projekty">
-    <div className="flex gap-2 m-auto mb-7">
-      <img src="/images/wave.svg" alt="fala" />
-      <h2 className="mb-2">Projekty</h2>
-      <img src="/images/wave.svg" alt="fala" />
-    </div>
+type Props = {
+  lang: string;
+};
 
-    {projects.map(({ name, description, link, github, image }) => (
-      <Project
-        name={name}
-        description={description}
-        link={link}
-        github={github}
-        image={image}
-      />
-    ))}
-  </section>
-);
+const Projects = (props: Props) => {
+  return (
+    <section className="flex flex-col items-center px-6 py-16" id="projekty">
+      <div className="flex gap-2 m-auto mb-7">
+        <img src="/images/wave.svg" alt="fala" />
+        <h2 className="mb-2">{projects[props.lang].title}</h2>
+        <img src="/images/wave.svg" alt="fala" />
+      </div>
+
+      {projects[props.lang].projs.map(
+        ({ name, description, link, github, image }) => {
+          return (
+            <Project
+              name={name}
+              description={description}
+              link={link}
+              github={github}
+              image={image}
+            />
+          );
+        }
+      )}
+    </section>
+  );
+};
 
 export default Projects;
