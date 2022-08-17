6a58760c2fab5a73db186732d5623fa2ad3a71f0
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2022-08-17 16:48:26 +0200

    about translation

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,14 @@
-const About = () => (
+import about from "../data/about.json";
+
+type Props = {
+  lang: string;
+};
+
+const About = ({ lang }: Props) => (
   <section className="flex gap-7 px-6 py-16 bg-background" id="omnie">
     <img className="self-end" src="/images/forrest.svg" alt="las" />
     <div>
-      <h2>O mnie</h2>
+      <h2>{about[lang].title}</h2>
       <p>
         Strony internetowe to moja pasja. Zajmuję się nimi prawie codziennie. Z
         moją pomocą możesz mieć własną stronę w internecie bez technicznych
