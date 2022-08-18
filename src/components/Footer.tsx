7748eacfeddf8cf6ca7b1d21973f8cd2cbbc4ff6
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2022-08-18 08:48:44 +0200

    footer translation done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,17 @@
-const Footer = () => (
+import footer from "../data/footer.json";
+
+type Props = {
+  lang: string;
+};
+
+const Footer = ({ lang }: Props) => (
   <footer className="bg-primary flex justify-between px-6 py-8 mt-[2rem]">
-    <img className="w-28" src="/images/logo-light.svg" alt="logo Alaska Web" />
-    <div className="flex  items-center">
+    <img
+      className="w-28"
+      src="/images/logo-light.svg"
+      alt={footer[lang].logoAlt}
+    />
+    <div className="flex items-center">
       <a
         className="p-[9px]"
         href="https://twitter.com/oliwierzgorniak"
@@ -10,7 +20,7 @@ const Footer = () => (
         <img
           className="w-[34px] h-[34px]"
           src="/images/Github-white.png"
-          alt="logo Githuba"
+          alt={footer[lang].github}
         />
       </a>
       <a
@@ -21,7 +31,7 @@ const Footer = () => (
         <img
           className="w-[34px]"
           src="/images/Twitter-white.svg"
-          alt="logo Twittera"
+          alt={footer[lang].twitter}
         />
       </a>
     </div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,23 +1,23 @@
-import projects from "../data/projects";
+import projects from "../data/projects.json";
 import Project from "./projects/Project";
 
 type Props = {
   lang: string;
 };
 
-const Projects = (props: Props) => {
+const Projects = ({ lang }: Props) => {
   return (
     <section
       className="flex flex-col items-center px-6 py-16"
-      id={projects[props.lang].sectionId}
+      id={projects[lang].sectionId}
     >
       <div className="flex gap-2 m-auto mb-7">
         <img src="/images/wave.svg" alt="fala" />
-        <h2 className="mb-2">{projects[props.lang].title}</h2>
+        <h2 className="mb-2">{projects[lang].title}</h2>
         <img src="/images/wave.svg" alt="fala" />
       </div>
 
-      {projects[props.lang].projs.map(
+      {projects[lang].projects.map(
         ({ name, description, link, github, image }) => {
           return (
             <Project
